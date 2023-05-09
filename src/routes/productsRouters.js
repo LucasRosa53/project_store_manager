7708046bed56b18533673e47677a02b3ce6504cd
LucasRosa53 <lucasrosa223@hotmail.com>
2023-05-09 09:46:39 -0300

    resolvido requisito 4

--- src/routes/productsRouters.js
+++ src/routes/productsRouters.js
@@ -2,9 +2,10 @@ const express = require('express');
 
 const productsRouters = express.Router();
 const productsController = require('../controllers/productsController');
+const { validateName } = require('../middlewares/validateName');
 
 productsRouters.get('/', productsController.getAll);
 productsRouters.get('/:id', productsController.getProductById);
-productsRouters.post('/', productsController.productRegister);
+productsRouters.post('/', validateName, productsController.productRegister);
 
 module.exports = productsRouters;
